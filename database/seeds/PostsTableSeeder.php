f82ee18f739ddf29c2562b3c3b1dc6a304066d07
Marco-Gagliano <brazil_94@hotmail.it>
2022-07-05 17:54:49 +0200

    popolato-database-funzione-statica-slug

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class PostsTableSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 20; $i++){
+            $new_post = new Post();
+            $new_post->title = $faker->sentence(4);
+            $new_post->slug = Post::generateSlug($new_post->title);
+            $new_post->description = $faker->text();
+            $new_post->save();
+        }
     }
 }
